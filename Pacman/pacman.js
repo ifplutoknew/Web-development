a98e15a6901c454d5e7bd3000cc2812d73c6094b
ifplutoknew <BLYCHO001@myuct.ac.za>
2026-08-06 20:47:15 +0200

    pacman game

---
+++ Pacman/pacman.js
@@ -0,0 +1,121 @@
+//board
+let board;
+const rowCount =  21;
+const columnCount = 19;
+const tileSize = 32;
+const boardWidth = columnCount*tileSize; //19*32px
+const boardHeight = rowCount*tileSize;   //21*32px
+let context;
+
+//images
+let blueGhostImage;
+let orangeGhostImage;
+let pinkGhostImage;
+let redGhostImage;
+let pacmanUpImage;
+let pacmanDownImage;
+let pacmanLeftImage;
+let pacmanRightImage;
+let wallImage;
+
+//X = wall, O = skip, P = pac man, ' ' = food
+//Ghosts: b = blue, o = orange, p = pink, r = red
+const tileMap = [
+    "XXXXXXXXXXXXXXXXXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X                 X",
+    "X XX X XXXXX X XX X",
+    "X    X       X    X",
+    "XXXX XXXX XXXX XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXrXX X XXXX",
+    "O       bpo       O",
+    "XXXX X XXXXX X XXXX",
+    "OOOX X       X XOOO",
+    "XXXX X XXXXX X XXXX",
+    "X        X        X",
+    "X XX XXX X XXX XX X",
+    "X  X     P     X  X",
+    "XX X X XXXXX X X XX",
+    "X    X   X   X    X",
+    "X XXXXXX X XXXXXX X",
+    "X                 X",
+    "XXXXXXXXXXXXXXXXXXX" 
+];
+
+const walls = new Set();
+const foods = new Set();
+const ghosts =  new Set();
+let pacman;
+
+
+window.onload = function(){
+    board = document.getElementById('board');
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext('2d');  //used for drawing on the board
+
+    loadImages();
+}
+
+
+
+function loadImages(){
+    wallImage = new Image();
+    wallImage.src = "images/wall.png";
+
+    blueGhostImage = new Image();
+    blueGhostImage.src = "images/blueGhost.png";
+    orangeGhostImage =  new Image();
+    orangeGhostImage.src = "images/orangeGhost.png";
+    pinkGhostImage = new Image();
+    pinkGhostImage.src = "images/pinkGhost.png";
+    redGhostImage = new Image();
+    redGhostImage.src = "images/redGhost.png"
+
+    pacmanUpImage =  new Image();
+    pacmanUpImage.src = "images/pacmanUp.png";
+    pacmanDownImage = new Image();
+    pacmanDownImage.src = "images/pacmanDown.png";
+    pacmanLeftImage = new Image();
+    pacmanLeftImage.src = "images/pacmanLeft.png";
+    pacmanRightImage = new Image();
+    pacmanRightImage.src = "images/pacmanRight.png";
+}
+
+function loadMap(){
+    walls.clear();
+    foods.clear();
+    ghosts.clear();
+
+    for (let r= 0; r < rowCount; r++){
+        for (let c=0; c < columnCount; c++){
+            const row = tileMap[r];
+            const tileMapChar = row[c];
+
+            const x = c*tileSize;
+            const y = r*tileSize;
+
+            if (tileMapChar == 'X'){
+                //block wall
+                const wall =  new Block(wallImage, x, y, tileSize, tileSize);
+                walls.add(wall);
+            }
+        }
+    }
+}
+
+class Block {
+    constructor(image, x, y, width, height){
+        this.image = image;
+        this.x = x;
+        this.y =y ;
+        this.width = width;
+        this.height = height;
+
+        this.startX = x;
+        this.startY = y;
+
+    }
+}
